ad5dbc610c4b0a8257db7b281501bc9005397d25
vipulgarg1998 <garg.vipul7@gmail.com>
2023-09-03 22:35:56 +0530

    data structure added

---
+++ utils/data.py
@@ -0,0 +1,23 @@
+class Data:
+    def __init__(self) -> None:
+        self.image_loc = None
+        
+        self.left_img = None
+        self.right_img = None
+        
+        self.left_color_img = None
+        self.right_color_img = None
+        
+        self.index = None
+        
+        self.abs_pose = None
+        self.abs_pose_right = None
+        self.rel_pose = None
+        self.rel_pose_right = None
+        
+        self.t = None
+        self.label_img = None
+        
+        
+        
+    
